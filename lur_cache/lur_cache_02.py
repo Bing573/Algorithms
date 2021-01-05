d8eb0b69bd9294747ab426fd4f02b5319c4f3783
liunnnanbing <exformation@icloud.com>
2021-01-05 20:31:24 +0800

    fix

--- lur_cache/lur_cache_02.py
+++ lur_cache/lur_cache_02.py
@@ -29,8 +29,11 @@ class LurCache(object):
             if len(self.cache) > self.capacity:
                 poped = self.pop_head()
                 del self.cache[poped.key]
+                print(f'pop node {poped.key}')
+                print(self.cache.keys())
+                
             
-        
+    
     def get(self, key):
         if key not in self.cache:
             return -1
@@ -41,27 +44,29 @@ class LurCache(object):
     
     def append(self, node):
         tail_node = self.tail.prev
-        tail_node.next = node
         node.prev = tail_node
         node.next = self.tail
+        tail_node.next = node
         self.tail.prev = node
+        
         
     def pop_head(self):
         head_node = self.head.next
-        head_node.prev.next = self.head
-        self.head.next = head_node.next
+        self.head.next = self.head.next.next
+        self.head.next.prev = self.head
         return head_node
     
     def move_to_end(self, node):
         tail_node = self.tail.prev
         if tail_node.key == node.key:
             return
-        tail_node.next = node
-        node.prev = tail_node
         node.prev.next = node.next
         node.next.prev = node.prev
+        tail_node.next = node
+        node.prev = tail_node
         self.tail.prev = node
         node.next = self.tail
+        
 
         
 
@@ -77,20 +82,23 @@ class Node (object):
 class LurCacheTestCase(unittest.TestCase):
     
     def test(self):
-        lur_cache = LurCache(2)
-        lur_cache.put(1,1)
-        lur_cache.put(2,2)
-        self.assertEqual(1, lur_cache.get(1))
-        self.assertEqual(2, lur_cache.get(2))
-        
-        lur_cache.put(3,3)
-        self.assertEqual(-1, lur_cache.get(1))
-        
+        lur_cache = LurCache(3)
+        lur_cache.put(1, 1)
+        lur_cache.put(2, 2)
+        lur_cache.put(3, 3)
         lur_cache.put(4, 4)
-        self.assertEqual(-1, lur_cache.get(2))
-        self.assertEqual(3, lur_cache.get(3))
         self.assertEqual(4, lur_cache.get(4))
-        
+        self.assertEqual(3, lur_cache.get(3))
+        self.assertEqual(2, lur_cache.get(2))
+        self.assertEqual(-1, lur_cache.get(1))
+        lur_cache.put(5, 5)
+        self.assertEqual(-1, lur_cache.get(1))
+        self.assertEqual(2, lur_cache.get(2))
+        self.assertEqual(3, lur_cache.get(3))
+        self.assertEqual(-1, lur_cache.get(4))
+        self.assertEqual(5, lur_cache.get(5))
+
+
 
 if __name__ == '__main__':
     unittest.main()
